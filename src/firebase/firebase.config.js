e300f8ddefbbab655347914237b6f83a8eb42d03
mk-saadi <mksaadi820@gmail.com>
2023-05-02 03:46:46 +0600

    2nd commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,17 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDn7NGynnlgfpMN40pLgin1WJws9wZG_Fg",
-    authDomain: "assignment-10-5ba07.firebaseapp.com",
-    projectId: "assignment-10-5ba07",
-    storageBucket: "assignment-10-5ba07.appspot.com",
-    messagingSenderId: "265812561907",
-    appId: "1:265812561907:web:86eeccdc70e3324c07cc86",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
